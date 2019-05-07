631b473f89006fcec98d8941b0d76f39aed108ca
Last Year <soymilk@airmail.cc>
2019-05-07 14:52:44 -0400

    mutating bubble sort

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,5 +3,27 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-  
+
+    let sorted = false;
+    while(!sorted) {
+
+	sorted = true;
+	for(let i = 0; i < numArray.length - 1; i++) {
+
+	    if(numArray[i] > numArray[i + 1]) {
+
+		rotate(numArray, i, i + 1);
+		sorted = false;
+	    }
+	}
+    }
+
+    return numArray;
+}
+
+function rotate(arr, i, j) {
+
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
 }
