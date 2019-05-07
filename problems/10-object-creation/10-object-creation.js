627972cbaba162296b9928c2fe0bcea086d33b2b
Last Year <soymilk@airmail.cc>
2019-05-07 16:37:50 -0400

    example objects with test output

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,28 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let objLiteral = {name: "hi", age: 1000};
+
+function cons(name, age) {
+
+    this.name = name;
+    this.age = age;
+}
+
+let objConstructed = new cons("hi", "young");
+
+class NameAge {
+
+    constructor(name, age) {
+
+	this.name = name;
+	this.age = age;
+    }
+}
+
+let objClass = new NameAge("hi", "old");
+
+console.log(objLiteral);
+console.log(objConstructed);
+console.log(objClass);
