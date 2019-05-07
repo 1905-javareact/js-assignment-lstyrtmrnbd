b0c486c58db9fcde1163dde4d0f0bed8e6a8031f
Last Year <soymilk@airmail.cc>
2019-05-07 15:38:27 -0400

    recursive isEven, sorry for all the ternaries

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,19 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+
+    return (someNum >= 0) ? positiveEven(someNum) : negativeEven(someNum);
+ }
+
+function positiveEven(n) {
+
+    return (n === 0) ? true :
+	(n < 0) ? false : positiveEven(n - 2);
+}
+
+function negativeEven(n) {
+
+    return (n === 0) ? true :
+	(n > 0) ? false : negativeEven(n + 2);
+
 }
