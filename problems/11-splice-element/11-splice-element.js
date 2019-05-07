776fef42b7e9b51d4c40d2148a85973f8cccf603
Last Year <soymilk@airmail.cc>
2019-05-07 16:58:27 -0400

    slice and concat spliceElement implementation

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,6 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    
+    return someArr.slice(0, index).concat(someArr.slice(index+1, someArr.length - 1));
 }
