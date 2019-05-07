b5439acc6b36a87aaf18293d031fbfdc896caa23
Last Year <soymilk@airmail.cc>
2019-05-07 15:46:03 -0400

    simple isPalindrome, cribbed reverseStr

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,17 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+
+    return someStr === reverseStr(someStr);
 }
+
+function reverseStr(someStr) {
+
+    let result = "";
+    for(let i = someStr.length - 1; i >= 0; i--) {
+
+	result += someStr[i];
+    }
+    return result;
+}
+
