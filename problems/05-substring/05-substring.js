4bf72c09f9bf6292772f82caee06f6ba97355407
Last Year <soymilk@airmail.cc>
2019-05-07 15:25:44 -0400

    naive substring

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -5,4 +5,32 @@ If incorrect input is entered, throw an exception stating the input was incorrec
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
 
+    validIndices(startIndex, endIndex, someStr.length);
+
+    let result = "";
+    for(let i = startIndex; i < endIndex; i++) {
+
+	result += someStr[i];
+    }
+
+    return result;
+}
+
+function checkBounds(n, length) {
+
+    if(n < 0 || n >= length) {
+
+	throw(`Error: index ${n} out of bounds`);
+    }
+}
+
+function validIndices(start, end, length) {
+
+    checkBounds(start, length);
+    checkBounds(end, length);
+
+    if(start > end) {
+
+	throw("Start index larger than end");
+    }
 }
