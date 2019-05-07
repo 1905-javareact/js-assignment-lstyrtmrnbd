3682c80044df21fa9b6f680c09b43d58257dd6a5
Last Year <soymilk@airmail.cc>
2019-05-07 15:03:50 -0400

    naive string reverse

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,11 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+
+    let result = "";
+    for(let i = someStr.length - 1; i >= 0; i--) {
+
+	result += someStr[i];
+    }
+    return result;
 }
