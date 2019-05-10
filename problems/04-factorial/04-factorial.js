e48500a644f0528cfa03ef85bef20045f657727c
Last Year <soymilk@airmail.cc>
2019-05-10 16:17:20 -0400

    product, not sum

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -3,5 +3,7 @@ Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
 
-    return (sumNum === 0) ? sumNum : sumNum + factorial(sumNum - 1);
+    return (sumNum === 0) ? 1 : sumNum * factorial(sumNum - 1);
 }
+
+console.log(factorial(5));
